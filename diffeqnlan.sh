ed2a3dc626a709ae423e416df080b46c79c35bd4
fioriandrea <andrea.fiori.1998@gmail.com>
2020-05-08 10:40:56 +0200

    rename java template

--- diffeqnlan.sh
+++ diffeqnlan.sh
@@ -9,4 +9,4 @@ fi
 
 name=$(basename "$1")
 
-awk -v comp="$compiled" -v class="$name" '{sub("<<COMPILED>>", comp, $0); sub("<<CLASSNAME>>", class, $0); print}' ./Interpreter.java > "$name.java"
+awk -v comp="$compiled" -v class="$name" '{sub("<<COMPILED>>", comp, $0); sub("<<CLASSNAME>>", class, $0); print}' ./interpreterTemplate > "$name.java"
